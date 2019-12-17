9601186b5da083aec785a9bb872d73c09d092f43
zy <abc131def456ghi789@qq.com>
2019-12-18 02:57:55 +0800

    再提交

--- asp.net实训。/Controllers/OrtherController.cs
+++ asp.net实训。/Controllers/OrtherController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using asp.net实训_.Models;
 namespace asp.net实训_.Controllers
 {
 
@@ -12,6 +12,7 @@ namespace asp.net实训_.Controllers
     //管理员首页
     public class OrtherController : Controller
     {
+        safeDataContext db = new safeDataContext();
         // GET: Orther
         public ActionResult Index()
         {
@@ -22,6 +23,25 @@ namespace asp.net实训_.Controllers
         public ActionResult indexAdmin()//管理员首页
         {
             return View();
+        }
+        public JsonResult indexBack()
+        {
+            total t1=new total();
+            t1.chargeNum = new List<int>();
+            t1.unchargeNum = new List<int>();
+            t1.compalinNum = new List<int>();
+            t1.repairSolveNum = db.repairAdmin.Where(d => d.isSolve).Count();
+            t1.repairUnSolveNum = db.repairAdmin.Where(d => !d.isSolve).Count();
+            var cn = db.charge.Where(p => p.isPay).GroupBy(d => d.chargeDate.Month).Select(p => p.Count()).ToList();
+            var ucn = db.charge.Where(p => !p.isPay).GroupBy(d => d.chargeDate.Month).Select(p=>p.Count()).ToList();
+            var cnn = db.complain.GroupBy(d => d.complainDate.Month).Select(p=>p.Count()).ToList();
+            foreach(int i in cn)
+                t1.chargeNum.Add(i);
+            foreach (int i in ucn)
+                t1.unchargeNum.Add(i);
+            foreach (int i in cnn)
+                t1.compalinNum.Add(i);
+            return Json(t1, "text/html", JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ asp.net实训。/Models/total.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp.net实训_.Models
+{
+    public class total
+    {
+        //维修解决人数
+        public int repairSolveNum
+        {
+            get;
+            set;
+        }
+        //维修未解决人数
+        public int repairUnSolveNum
+        {
+            get;
+            set;
+        }
+        //每月收费人数
+        public List<int> chargeNum
+        {
+            get;
+            set;
+        }
+        //每月欠费人数
+        public List<int> unchargeNum
+        {
+            get;
+            set;
+        }
+        //每页投诉人数
+        public List<int> compalinNum
+        {
+            get;
+            set;
+        }
+    }
+}

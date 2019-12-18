b267bc4b8571396b4dce796ed49c8de868815e14
zy <abc131def456ghi789@qq.com>
2019-12-18 19:41:29 +0800

    呵呵

--- asp.net实训。/Controllers/UserController.cs
+++ asp.net实训。/Controllers/UserController.cs
@@ -3,25 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using asp.net实训_.Models;
 namespace asp.net实训_.Controllers
 {
     //业主
     //业主管理
     public class UserController : Controller
     {
+        safeDataContext db = new safeDataContext();
         // GET: User
         public string Index()
         {
             return "张跃制作的UserController";
         }
-        public ActionResult user()//业主个人信息页
+        //业主个人信息页
+        public ActionResult user()
         {
             return View();
         }
-        public ActionResult userAdmin()//管理业主页
+        //管理业主页
+        public ActionResult userAdmin()
         {
-            return View();
+            var u1 = db.user.Select(p => new users { userId = p.userId, userName = p.userName, userMail = p.userEmail, userPhone = p.userPhone, userAdress = p.userAddress, userPassword = p.userPassword });
+            return View(u1.ToList());
+            
+        }
+        //业主修改
+        public void userUpdate(users u)
+        {
+            var u1 = db.user.Where(p => p.userId == u.userId);
+            foreach(user us in u1)
+            {
+                us.userName = u.userName;
+                us.userPhone = u.userPhone;
+                us.userAddress = u.userAdress;
+                us.userEmail = u.userMail;
+            }
+            db.SubmitChanges();
+        }
+        //业主插入
+        public string userInsert(users u)
+        {
+            var uh = db.user.Where(p => p.userEmail == u.userMail);
+            if (uh != null && uh.Count() > 0)
+            {
+                return "失败";
+            }
+            else
+            {
+                user u1 = new user();
+                u1.userEmail = u.userMail;
+                u1.userAddress = u.userAdress;
+                u1.userName = u.userName;
+                u1.userPassword = u.userPassword;
+                u1.userPhone = u.userPhone;
+                db.user.InsertOnSubmit(u1);
+                db.SubmitChanges();
+            }
+           
+            return Json("");
         }
     }
 }
---
+++ asp.net实训。/Models/users.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp.net实训_.Models
+{
+    public class users
+    {
+        //业主编号
+        public int userId
+        {
+            get;
+            set;
+        }
+        //业主姓名
+        public string userName
+        {
+            get;
+            set;
+        }
+        //业主邮箱
+        public string userMail
+        {
+            get;
+            set;
+        }
+        //业主电话
+        public string userPhone
+        {
+            get;
+            set;
+        }
+        //业主地址
+        public string userAdress
+        {
+            get;
+            set;
+        }
+        //业主密码
+        public string userPassword
+        {
+            get;
+            set;
+        }
+    }
+}
